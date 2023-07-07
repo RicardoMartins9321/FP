7ce34c5c051d5292c17be6601271d5d170057fc1
RicardoMartins9321 <114387040+RicardoMartins9321@users.noreply.github.com>
2023-07-07 14:36:30 +0100

    Delete Aula01 directory

--- Aula01/idade.py
+++
@@ -1,9 +0,0 @@
-nome = input("Como te chamas? ")
-ano = int(input("Em que ano nasceste? "))
-idade = 2030-ano
-
-if 1900 < ano <2030:
-    resposta = print(nome, "fará", idade, "anos em 2030")
-else:
-    print("Por favor introduza um valor válido")
-
